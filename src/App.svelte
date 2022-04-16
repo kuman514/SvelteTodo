96147f603636984b63e22c6f3872039cb88a79a8
kumankoishi.py <7iggif@naver.com>
2022-04-16 21:47:45 +0900

    Added adding a todo

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,48 @@
 <script>
+	import Todo from './Todo.svelte';
+
 	let title = '';
 	let todos = [];
+	let id = 0;
+
+	function createTodo() {
+		/*
+		const newTodos = Array.from(todos);
+		newTodos.push({
+			id,
+			title
+		});
+		todos = newTodos;
+		*/
+		// Svelte에서는 자기자신을 할당하는 것이 반응성을 살리는 것이라 일반적이다?
+		todos.push({
+			id,
+			title
+		});
+		todos = todos;
+		title = '';
+		id++;
+	}
 </script>
 
-<input type="text" />
-<button>
+<input
+	type="text"
+	bind:value={title}
+	on:keydown={event => {
+		if (event.key === 'Enter') {
+			createTodo();
+		}
+	}}
+/>
+<button on:click={createTodo}>
 	Create Todo
 </button>
+{#each todos as todo}
+	<Todo todo={todo} />
+{/each}
+
+<style>
+	button {
+		background-color: aliceblue;
+	}
+</style>
---
+++ src/Todo.svelte
@@ -0,0 +1,22 @@
+<script>
+  // Props
+  export let todo;
+
+  function onEdit() {
+
+  }
+
+  function deleteTodo() {
+
+  }
+</script>
+
+<div>
+  {todo.title}
+  <button on:click={onEdit}>
+    Edit
+  </button>
+  <button on:click={deleteTodo}>
+    Delete
+  </button>
+</div>
